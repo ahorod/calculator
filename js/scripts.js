faec0b2a2f5088d6f1c35f3f9c282d8cc7469f89
ah and tu <tu@>
2017-03-20 13:29:19 -0700

    add different functions, BMI, temp. conversion, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,29 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var BMI = function(number1, number2) {
+  return number1 / (number2*number2) * 703;
+};
+
+
+var tempConversion = function(number1) {
+  return ((number1 - 32) * 5)/9;
+};
+
+var volumeConversion = function(numer1){
+  return number1 * 3.78541178;
+}
+
+//var number1 = parseInt(prompt("Enter a weight:"));
+//var number2 = parseInt(prompt("Enter a height:"));
+//var number1 = parseInt(prompt("Enter temprature in fahrenheit:"));
+var number1 = parseInt(prompt("Enter volume in gallons:"));
+alert("Volume in liters" + volumeConversionumber1));
